1801c63aa8504e3b9bf052df091ad992205fa3f2
agpiermarini <piermarini.andrew@gmail.com>
2018-07-18 21:35:45 -0600

    Add UserViews. Add UserViews.create. Add serializers.py, and UserSerializer

--- api/views.py
+++ api/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+from .serializers import FoodSerializer, MealSerializer
+from .models import Food, Meal
 
-# Create your views here.
+class UsersView(viewsets.ViewSet):
+
+    def create(self, requeset):
+        try:
+            queryset = User.objects.create(email=request.data['user'['email'], password=request.data['user']['password'])
+        except:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        serialzer = UserSerializer(queryset, many=False)
+        return Response(serializer.data)        
