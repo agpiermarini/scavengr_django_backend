be24ce81176543543177f10d96be6b1f6cabdff9
agpiermarini <piermarini.andrew@gmail.com>
2018-07-19 00:14:31 -0600

    Add static_root to settings. Adds static directory after running collect static

--- scavengr_django_backend/settings.py
+++ scavengr_django_backend/settings.py
@@ -133,3 +133,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Directory for collect static
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
