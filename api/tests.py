8e5ccd7eed388393a8ba500dbff354a7f538654d
agpiermarini <piermarini.andrew@gmail.com>
2018-07-18 21:20:04 -0600

    Add tests for user create endpoint

--- api/tests.py
+++ api/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from rest_framework.test import APIClient
 from rest_framework import status
-# from .models, import User
+from .models import User
 
 class UserModelTestCase(TestCase):
 
@@ -12,7 +12,7 @@ class UserModelTestCase(TestCase):
 
     def test_model_can_create_a_user(self):
         old_count = User.objects.count()
-        self.user.save
+        self.user.save()
         new_count = User.objects.count()
         new_user = User.objects.first()
 
@@ -20,3 +20,28 @@ class UserModelTestCase(TestCase):
         self.assertEqual(new_count, 1)
         self.assertEqual(new_user.email, self.email)
         self.assertEqual(new_user.password, self.password)
+
+class UserEndpointsTesetCase(TestCase):
+
+    def setUp(self):
+        self.client = APIClient()
+
+        self.email = "user@user.com"
+        self.password = "encrypted_password"
+        self.user = User(email=self.email, password=self.password)
+
+        self.email2 = "user2@user.com"
+        self.password2 = "encrypted_password2"
+        self.user2 = User(email=self.email2, password=self.password2)
+
+        self.email3 = "user3@user.com"
+        self.password3 = "encrypted_password3"
+        self.post_params = { 'user': { 'email': self.email3, 'password': self.password3 } }
+
+    def test_user_create_endpoint(self):
+        response = self.client.post('/api/v1/users/', self.post_params, format='json')
+        user = response.json()['user']
+
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(user['email'], self.email3)
+        self.assertEqual(user['password'], self.password3)
