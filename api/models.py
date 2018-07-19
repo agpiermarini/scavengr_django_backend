2fae64889ac9ec818c39bbcba6f397c9ea5f189e
agpiermarini <piermarini.andrew@gmail.com>
2018-07-18 21:07:29 -0600

    Add user model and migration. Update db.sqlite3

--- api/models.py
+++ api/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    email = models.CharField(max_length=100, blank=False)
+    password = models.CharField(max_length=100, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        ordering = ('id',)
