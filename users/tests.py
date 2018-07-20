5a8688787b79d59d9fe9c675b3d5eea4d0a75d2e
agpiermarini <piermarini.andrew@gmail.com>
2018-07-19 21:48:19 -0600

    Import DRF authoken module to tests

--- users/tests.py
+++ users/tests.py
@@ -2,6 +2,8 @@ from django.test import TestCase
 from rest_framework.test import APIClient
 from rest_framework import status
 from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
+
 
 class UserModelTestCase(TestCase):
 
