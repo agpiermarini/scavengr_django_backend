319960d24a029370426ec429482f4fec9365a2fc
agpiermarini <piermarini.andrew@gmail.com>
2018-07-19 23:00:10 -0600

    Update urls to path to accomodate new authentication endpoint

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,11 @@
-from django.urls import include, path, re_path
+from django.urls import include, path
 from rest_framework.urlpatterns import format_suffix_patterns
 from rest_framework.authtoken import views as drf_views
 from .views import UserView
 
 urlpatterns = [
-    re_path(r'^', UserView.as_view({'post': 'create'})),
-    re_path(r'^authenticate/', drf_views.obtain_auth_token),
+    path('', UserView.as_view({'post': 'create'})),
+    path('authenticate/', drf_views.obtain_auth_token),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
