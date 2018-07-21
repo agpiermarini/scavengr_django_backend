798353a3696928611ef675128d759babc148004f
agpiermarini <piermarini.andrew@gmail.com>
2018-07-21 14:32:20 -0600

    Add assertions for id in scavenger hunt response hash. Add tests for scavenger hunt update endpoint

--- scavengerhunts/tests.py
+++ scavengerhunts/tests.py
@@ -26,6 +26,7 @@ class ScavengerHuntEndpointTestCase(TestCase):
         response = self.client.post('/api/v1/scavenger_hunts/', data, format='json')
 
         self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.data['id'], 1)
         self.assertEqual(response.data['name'], self.name)
         self.assertEqual(response.data['description'], self.description)
         self.assertEqual(response.data['user_id'], self.user.id)
@@ -48,5 +49,42 @@ class ScavengerHuntEndpointTestCase(TestCase):
                 }
 
         response = self.client.post('/api/v1/scavenger_hunts/', data, format='json')
+
+        self.assertEqual(response.status_code, 400)
+
+    def test_scavenger_hunt_update_endpoint(self):
+        scavenger_hunt = ScavengerHunt.objects.create(id=1, name="old_name", description="old_description", user_id=self.user.id)
+
+        data = {
+                'name': self.name,
+                'description': self.description
+                }
+
+        response = self.client.put('/api/v1/scavenger_hunts/1', data, format='json')
+
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.data['id'], 1)
+        self.assertEqual(response.data['name'], self.name)
+        self.assertEqual(response.data['description'], self.description)
+        self.assertEqual(response.data['user_id'], self.user.id)
+        self.assertEqual(response.data['username'], self.user.username)
+
+    def test_scavenger_hunt_update_endpoint_no_name(self):
+        data = {
+                'name': "",
+                'description': self.description
+                }
+
+        response = self.client.put('/api/v1/scavenger_hunts/1', data, format='json')
+
+        self.assertEqual(response.status_code, 400)
+
+    def test_scavenger_hunt_update_endpoint_no_description(self):
+        data = {
+                'name': self.name,
+                'description': ""
+                }
+
+        response = self.client.put('/api/v1/scavenger_hunts/1', data, format='json')
 
         self.assertEqual(response.status_code, 400)
