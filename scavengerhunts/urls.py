c6cccc5a3e9c811ad0539330ff3ee478de4eb6bd
agpiermarini <piermarini.andrew@gmail.com>
2018-07-21 14:35:13 -0600

    Add scavenger hunt update endpoints to url

--- scavengerhunts/urls.py
+++ scavengerhunts/urls.py
@@ -4,6 +4,7 @@ from .views import ScavengerHuntView
 
 urlpatterns = [
     path('', ScavengerHuntView.as_view({'post': 'create'})),
+    path('<int:id>', ScavengerHuntView.as_view({'patch': 'update', 'put': 'update'})),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
