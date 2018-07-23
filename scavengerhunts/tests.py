51d82f4925cd9a55de5901f6112c404141ba5675
agpiermarini <piermarini.andrew@gmail.com>
2018-07-23 11:43:12 -0600

    Add test for user specific scavenger hunt index endpoint. Add additional scavenger hunt (with a different user id than the previous two) object to scavenger hunts index

--- scavengerhunts/tests.py
+++ scavengerhunts/tests.py
@@ -9,6 +9,7 @@ class ScavengerHuntEndpointTestCase(TestCase):
 
     def setUp(self):
         self.user = User.objects.create(username='testuser', email='test@email.com', password='password')
+        self.user2 = User.objects.create(username='testuser2', email='test2@email.com', password='password')
         self.token = Token.objects.get(user=self.user)
 
         self.client = APIClient()
@@ -170,13 +171,15 @@ class ScavengerHuntEndpointTestCase(TestCase):
 
         self.assertEqual(response.status_code, 404)
 
-    def test_scavenger_hunt_show_endpoint(self):
-        scavenger_hunt1 = ScavengerHunt.objects.create(id=1, name=self.name, description=self.description, user_id=self.user.id)
-        scavenger_hunt1 = ScavengerHunt.objects.create(id=2, name="second_hunt", description="second_description", user_id=self.user.id)
+    def test_scavenger_hunt_index_endpoint(self):
+        ScavengerHunt.objects.create(id=1, name=self.name, description=self.description, user_id=self.user.id)
+        ScavengerHunt.objects.create(id=2, name="second_hunt", description="second_description", user_id=self.user.id)
+        ScavengerHunt.objects.create(id=3, name="third_hunt", description="third_description", user_id=self.user2.id)
+
         response = self.client.get('/api/v1/scavenger_hunts/')
 
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(len(response.json()), 2)
+        self.assertEqual(len(response.json()), 3)
         self.assertEqual(response.json()[0]['id'], 1)
         self.assertEqual(response.json()[0]['name'], self.name)
         self.assertEqual(response.json()[0]['description'], self.description)
@@ -187,6 +190,11 @@ class ScavengerHuntEndpointTestCase(TestCase):
         self.assertEqual(response.json()[1]['description'], "second_description")
         self.assertEqual(response.json()[1]['user_id'], self.user.id)
         self.assertEqual(response.json()[1]['username'], self.user.username)
+        self.assertEqual(response.json()[2]['id'], 3)
+        self.assertEqual(response.json()[2]['name'], "third_hunt")
+        self.assertEqual(response.json()[2]['description'], "third_description")
+        self.assertEqual(response.json()[2]['user_id'], self.user2.id)
+        self.assertEqual(response.json()[2]['username'], self.user2.username)
 
     def test_scavenger_hunt_delete_endpoint(self):
         scavenger_hunt1 = ScavengerHunt.objects.create(id=1, name=self.name, description=self.description, user_id=self.user.id)
@@ -203,3 +211,22 @@ class ScavengerHuntEndpointTestCase(TestCase):
         response = self.client.delete('/api/v1/scavenger_hunts/3')
 
         self.assertEqual(response.status_code, 404)
+
+    def test_user_specific_scavenger_hunt_index_endpoint(self):
+        ScavengerHunt.objects.create(id=1, name=self.name, description=self.description, user_id=self.user.id)
+        ScavengerHunt.objects.create(id=2, name="second_hunt", description="second_description", user_id=self.user.id)
+        ScavengerHunt.objects.create(id=3, name="third_hunt", description="third_description", user_id=self.user2.id)
+        response = self.client.get('/api/v1/users/1/scavenger_hunts/')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.json()), 2)
+        self.assertEqual(response.json()[0]['id'], 1)
+        self.assertEqual(response.json()[0]['name'], self.name)
+        self.assertEqual(response.json()[0]['description'], self.description)
+        self.assertEqual(response.json()[0]['user_id'], self.user.id)
+        self.assertEqual(response.json()[0]['username'], self.user.username)
+        self.assertEqual(response.json()[1]['id'], 2)
+        self.assertEqual(response.json()[1]['name'], "second_hunt")
+        self.assertEqual(response.json()[1]['description'], "second_description")
+        self.assertEqual(response.json()[1]['user_id'], self.user.id)
+        self.assertEqual(response.json()[1]['username'], self.user.username)
