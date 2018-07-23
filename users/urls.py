7d827938c7bbe38e56d1ffa34df4db635a1e6dd5
agpiermarini <piermarini.andrew@gmail.com>
2018-07-23 11:45:20 -0600

    Add users/:id/scavenger_hunts url

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,11 @@
 from django.urls import include, path
 from rest_framework.urlpatterns import format_suffix_patterns
 from rest_framework.authtoken import views as drf_views
-from .views import UserCreateView, CustomAuthToken
+from .views import UserCreateView, UserView, CustomAuthToken
 
 urlpatterns = [
     path('', UserCreateView.as_view({'post': 'create'})),
+    path('<id>/scavenger_hunts/', UserView.as_view({'get': 'index'})),
     path('authenticate/', CustomAuthToken.as_view()),
 ]
 
