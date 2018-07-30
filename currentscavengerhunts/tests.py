b2fe57300f07b5c76ebe60799dceb9406bbb2687
agpiermarini <piermarini.andrew@gmail.com>
2018-07-30 14:35:11 -0600

    Add test for creating new currentscavengerhunt. Update currentscavengerhunts urls module

--- currentscavengerhunts/tests.py
+++ currentscavengerhunts/tests.py
@@ -1,7 +1,8 @@
 from django.test import TestCase
 from rest_framework.test import APIClient
 from rest_framework import status
-from scavengerhunts.models import ScavengerHunt, CurrentScavengerHunt
+from scavengerhunts.models import ScavengerHunt
+from currentscavengerhunts.models  import CurrentScavengerHunt
 from django.contrib.auth.models import User
 from rest_framework.authtoken.models import Token
 
